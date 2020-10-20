bf70994b59b651d54b4800fb15ee10bff82fd1ba
Shrinidhi SS <ss.shrinidhi.98@gmail.com>
2020-10-20 16:27:41 +0530

    User registration and Login authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
+    path('register/',views.registerPage,name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+
     path('',views.home,name='home'),
     path('products/',views.products,name='products'),
     path('customer/<int:pk>/',views.customer,name='customer'),
